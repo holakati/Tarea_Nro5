9e2e12581759e79a0ee34fbae599f4884ffa7435
holakati <katiuska.hormazabalc@utem.cl>
2019-01-05 15:20:34 -0300

    actualización

--- TareaFinal.R
+++ TareaFinal.R
@@ -9,7 +9,7 @@ library('rvest')
 archivo <-'tarea5.html'
 
 #Leyendo el html del archivo
-webpage <-read_html (archivo)
+webpage <- read_html (archivo)
 
 #Extrayendo contenido en la clase justificado
 contenidoWebNoticia <-html_nodes(webpage,'p')
@@ -35,6 +35,8 @@ textoNoticia <- gsub("â\u0080\u009c","",textoNoticia)
 textoNoticia <- gsub("â\u0080\u009d","",textoNoticia)
 textoNoticia <- gsub("Ã","í",textoNoticia)
 textoNoticia <- gsub("í¡","á",textoNoticia)
+textoNoticia <- gsub("[.]","",textoNoticia)
+textoNoticia <- gsub("[,]","",textoNoticia)
 
 #viendo a priori la info en la variable textoNoticia
 print(textoNoticia)
@@ -51,3 +53,28 @@ tablaPalabras <- table(unlistNoticias)
 
 #pasando información a unda data frame
 dfPalabrasNoticia <- as.data.frame(tablaPalabras)
+
+#Almacenando la información en csv
+write.csv(dfPalabrasNoticia, file="PalabrasNoticia.csv")
+
+#o en un txt
+write.table(dfPalabrasNoticia, file="PalabrasNoticias.txt")
+
+###### Extracción información tabla, buscar en wikipedia tf-IDF
+
+#Extrayendo el contenido de la tabla a través del tag table
+tablaProductosExtraida <- html_nodes(webpage, '.Productos')
+
+#Extracción de el contenido de las tablas usando el tag table
+contenedorDeTablas <- html_nodes(tablaProductosExtraida, 'table')
+
+#Extracción información tabla 1
+tabla1 <- html_table(contenedorDeTablas [1][[1]])
+tabla2 <-html_table(contenedorDeTablas[2][[1]])
+#Viendo el contenido de la posición 1,2 de la tabla
+print(contenedorDeTablas)
+
+install.packages('ggplot2')
+
+#graficando los productos
+library(ggplott2)
